4dc99e35f2e2fd8d4105137211ddff57c63bfbc5
ngel rojas p <aomarrojasp@gmail.com>
2020-05-13 00:49:22 -0300

    add panel user

--- src/pages/panel-user/index.tsx
+++ src/pages/panel-user/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
-import {Grid, Row, Col} from 'react-styled-flexboxgrid'
-import {TabList} from 'react-tabs'
+import {Grid, Row} from 'react-styled-flexboxgrid'
 import Profile from './panel-profile'
-import {Content, Title, TabItem, TabContent, TabPanels} from './styles'
+import {Content, TabItem, TabContent, TabPanels, TabLists} from './styles'
 
 const PanelUser: React.FC = () => {
     return (
         <Content>
             <Grid>
                 <Row>
-                    <Col xs={12}>
-                        <Row center="xs">
-                            <Col xs={6}>
-                                <Title>PANEL DE USUARIO</Title>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-
-                <Row>
                     <TabContent>
-                        <TabList>
+                        <TabLists>
                             <TabItem>perfil</TabItem>
                             <TabItem>mis proyectos</TabItem>
-                        </TabList>
+                        </TabLists>
                         <TabPanels>
                             <Profile />
                         </TabPanels>
