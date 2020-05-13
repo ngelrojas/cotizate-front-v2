f3efe7e2f17c4f198a1cdb4f36c787ac136d860f
ngel rojas p <aomarrojasp@gmail.com>
2020-05-13 00:52:28 -0300

    add styles to panel personal profile

---
+++ src/pages/panel-user/panel-profile/styles/index.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Content = styled.main`
+    height: auto;
+    min-height: 100%;
+    padding: 0em 1em 4em 1em;
+    box-sizing: border-box;
+    width: 100%;
+`
+
+export const Title = styled.h1`
+    text-transform: uppercase;
+    font-size: 18px;
+    color: #f37a22;
+`
+
+export const Label = styled.label`
+    margin: 5%;
+    float: left;
+    width: 33%;
+`
+
+export const Form = styled.form`
+    width: 100%;
+    margin: 3%;
+`
+
+export const Input = styled.input`
+    border: 0;
+    border-bottom: 1px solid #f37a22;
+    padding: 1%;
+    margin: 2%;
+    width: 100%;
+`
+export const FormSubTitle = styled.p`
+    text-transform: uppercase;
+    width: 100%;
+    color: #007bff;
+    margin-bottom: 4%;
+    font-size: 14px;
+`
