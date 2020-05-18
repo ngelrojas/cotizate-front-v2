a4d36ae47bf6940c6286aed4e0805095fd19b6a1
ngel rojas p <aomarrojasp@gmail.com>
2020-05-18 16:23:59 -0300

    pre-production

---
+++ build/precache-manifest.57609784a479c1029b3cdae9481599e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8003e42cbe9b54d93e38242956b7dcb1",
+    "url": "/ngelrojas/cotizate-front-v2/index.html"
+  },
+  {
+    "revision": "bb1db59008569e797e0d",
+    "url": "/ngelrojas/cotizate-front-v2/static/js/2.99d283c2.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/ngelrojas/cotizate-front-v2/static/js/2.99d283c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5e264e8d5743cfb251",
+    "url": "/ngelrojas/cotizate-front-v2/static/js/main.1aa8c57c.chunk.js"
+  },
+  {
+    "revision": "3350403923f1d7ea0c43",
+    "url": "/ngelrojas/cotizate-front-v2/static/js/runtime-main.f15f3533.js"
+  }
+]);
