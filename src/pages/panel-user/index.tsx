20a337a1eb6b0e847ec5546ff3ea2ef363d62103
ngel rojas <aomarrojasp@gmail.com>
2020-08-10 23:37:30 -0300

    update auth

--- src/pages/panel-user/index.tsx
+++ src/pages/panel-user/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {Grid, Row} from 'react-styled-flexboxgrid'
+import {useHistory} from 'react-router-dom'
 import Profile from './panel-profile'
 import ProfilePersonal from './panel-personal'
 import ProfileCompany from './panel-company'
 import {Content, TabItem, TabContent, TabPanels, TabLists} from './styles'
+import {CheckAuthentication} from '../../redux/auth'
 
-const PanelUser: React.FC = () => {
+const PanelUser: React.FC= () => {
+    let history = useHistory()
+    React.useEffect(()=>{
+        if(!CheckAuthentication()){
+            history.push('/')
+        }
+    },[])
+
     return (
         <Content>
             <Grid>
