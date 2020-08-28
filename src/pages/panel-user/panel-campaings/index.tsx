4731ea6e463a0f013fd4551ee92f0917a0009ea7
ngel rojas <aomarrojasp@gmail.com>
2020-08-27 23:26:29 -0300

    add module update-campaing

--- src/pages/panel-user/panel-campaings/index.tsx
+++ src/pages/panel-user/panel-campaings/index.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Loading from '../../../components/loading'
 import {Grid, Row, Col} from 'react-styled-flexboxgrid'
-import {Content, H1, Title, Table, Th, BtnCreate} from './styles'
+import {Content, H1, Title, Table, Th, Td, BtnCreate,
+        BtnUpdate,
+        BtnDelete
+       } from './styles'
 import {Campaings} from '../../../userCampaings'
 
 type FormData = {
@@ -30,6 +33,17 @@ const PanelCampaing: React.FC<Iauth> = ({authenticated, currentUser}) => {
     const [isData, setIsData] = React.useState(true)
     let token = window.sessionStorage.getItem('token')
     let campaing = new Campaings(token)
+
+    const formattDate = (date_in_parse:Date)=>{
+        var date_parse = new Date(date_in_parse)
+        var day = date_parse.getDate()
+        var month = date_parse.getMonth()
+        var year = date_parse.getFullYear()
+        var complete_date = day +'/'+month+'/'+year 
+        return complete_date 
+
+    }
+
     React.useEffect(()=>{
         campaing.listCampaings().then(resp => {
             console.log(resp.data.data)
@@ -82,35 +96,36 @@ const PanelCampaing: React.FC<Iauth> = ({authenticated, currentUser}) => {
                         <tbody>
                             {dataCamp ? (
                                 dataCamp.map((camp: any) => (
+                                    
                                     <tr key={camp.id}>
-                                        <td>{camp.title}</td>
-                                        <td>
-                                                {camp.created_at}
-                                        </td>
-                                        <td>{camp.public_at}</td>
-                                        <td>{camp.qty_day}</td>
-                                        <td>{camp.amount}</td>
-                                        <td>
+                                        <Td>{camp.title}</Td>
+                                        <Td>
+                                            {formattDate(camp.created_at)}
+                                        </Td>
+                                        <Td>{formattDate(camp.public_at)}</Td>
+                                        <Td>{camp.qty_day}</Td>
+                                        <Td>{camp.amount}</Td>
+                                        <Td>
                                             {camp.status}
-                                        </td>
-                                        <td>
-                                        <button>actualizar</button>
-                                        <button>eliminar</button>
-                                        </td>
+                                        </Td>
+                                        <Td>
+                                        <BtnUpdate to={`actualizar-campania/${camp.id}`}>actualizar</BtnUpdate> 
+                                        <BtnDelete to="#">eliminar</BtnDelete>
+                                        </Td>
                                     </tr>
                                 ))
                             ) : (
                                 <tr>
-                                    <td>
+                                    <Td>
                                        <Loading message="cargando campañas" />
-                                    </td>
+                                    </Td>
                                 </tr>
                             )}
                         </tbody>
                     ) : (
                         <tbody>
                             <tr>
-                                <td colSpan={5}>no existen datos</td>
+                                <Td colSpan={5}>no existen datos</Td>
                             </tr>
                         </tbody>
                     )}
