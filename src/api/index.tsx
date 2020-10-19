fa15fa5f0c0de41e82af76c8f1b894bdbe2ab9c2
ngel rojas <aomarrojasp@gmail.com>
2020-10-19 19:16:28 -0300

    update files

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// const URL_PROD = `http://35.226.214.154:1337/api/v2/`
+const URL_PROD = `http://35.226.214.154:1337/api/v2/`
 // const URL_PROD = `http://127.0.0.1:1337/api/v2/`
-const URL_PROD = `http://127.0.0.1:8000/api/v2/`
+// const URL_PROD = `http://127.0.0.1:8000/api/v2/`
 
 export default axios.create({
     baseURL: URL_PROD
