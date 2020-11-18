93b8ab7d96bd3536b8db7bd4f241c1ae251b4d72
ngel rojas <aomarrojasp@gmail.com>
2020-11-18 17:58:39 -0300

    add url in path images server

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const URL_PROD = `http://34.121.237.122:8000/api/v2/`
-const URL_PROD = `http://127.0.0.1:8000/api/v2/`
+const URL_PROD = `http://34.121.237.122:8000/api/v2/`
+//const URL_PROD = `http://127.0.0.1:8000/api/v2/`
 
 export default axios.create({
     baseURL: URL_PROD
