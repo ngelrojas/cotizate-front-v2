abfb89babb082d1eb8d059a74d86292994eb2adc
ngel rojas <aomarrojasp@gmail.com>
2020-11-17 23:31:09 -0300

    update profiles company

--- src/api-img/index.tsx
+++ src/api-img/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const URL_PROD = `http://35.225.59.226:9000/api/v1/`
-//const URL_PROD = `http://127.0.0.1:9000/api/v1/`
+//const URL_PROD = `http://35.225.59.226:9000/api/v1/`
+const URL_PROD = `http://127.0.0.1:9000/api/v1/`
 
 export default axios.create({
     baseURL: URL_PROD
